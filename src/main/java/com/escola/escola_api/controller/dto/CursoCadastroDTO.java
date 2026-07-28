a50d5ba1728590154a69e09be03bc5f3e9750660
Miguel Wainstein <miguel.wainst@gmail.com>
2026-07-28 12:02:26 -0300

    feat: criando diferentes DTOs para curso

--- src/main/java/com/escola/escola_api/controller/dto/CursoCadastroDTO.java
+++ src/main/java/com/escola/escola_api/controller/dto/CursoCadastroDTO.java
@@ -1,13 +1,10 @@
 package com.escola.escola_api.controller.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
-public record CursoDTO(
-        String id,
+public record CursoCadastroDTO(
         @NotBlank(message = "O nome não pode ser nulo.")
         String nome,
-        Integer cargaHoras,
-        String usuarioAtualizacao
+        Integer cargaHoras
 ) {
 }
---
+++ src/main/java/com/escola/escola_api/controller/dto/CursoPesquisaDTO.java
@@ -0,0 +1,9 @@
+package com.escola.escola_api.controller.dto;
+
+public record CursoPesquisaDTO(
+        String id,
+        String nome,
+        Integer cargaHoras,
+        String UsuarioAtualizacao
+) {
+}
