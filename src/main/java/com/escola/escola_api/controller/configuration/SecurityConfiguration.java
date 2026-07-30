3642f0c1a8e6763e8be56e32a8ceadd15fff06c1
Miguel Wainstein <miguel.wainst@gmail.com>
2026-07-29 23:51:35 -0300

    feat: implementando CustomUserDetailService para autenticação de usuários

--- src/main/java/com/escola/escola_api/controller/configuration/SecurityConfiguration.java
+++ src/main/java/com/escola/escola_api/controller/configuration/SecurityConfiguration.java
@@ -1,11 +1,14 @@
 package com.escola.escola_api.controller.configuration;
 
+import com.escola.escola_api.security.CustomUserDetailService;
+import com.escola.escola_api.service.UsuarioService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -30,5 +33,10 @@ public class SecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UsuarioService usuarioService) {
+        return new CustomUserDetailService(usuarioService);
     }
 }
---
+++ src/main/java/com/escola/escola_api/security/CustomUserDetailService.java
@@ -0,0 +1,28 @@
+package com.escola.escola_api.security;
+
+import com.escola.escola_api.model.entity.Usuario;
+import com.escola.escola_api.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UsuarioService usuarioService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuarioService.obterPorUsername(username);
+        if (usuario == null) {
+            throw new UsernameNotFoundException("Usuário não encontrado");
+        }
+        return User.builder()
+                .username(usuario.getUsername())
+                .password(usuario.getSenha())
+                .roles(usuario.getRoles().toArray(String[]::new))
+                .build();
+    }
+}
