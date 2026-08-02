50dbc2267f81743dc95d8f3aa6d4e803dec43c61
Miguel Wainstein <miguel.wainst@gmail.com>
2026-08-02 11:35:09 -0300

    feat: adicionando validação de usuário antes de salvar no repositório

--- src/main/java/com/escola/escola_api/service/UsuarioService.java
+++ src/main/java/com/escola/escola_api/service/UsuarioService.java
@@ -2,6 +2,7 @@ package com.escola.escola_api.service;
 
 import com.escola.escola_api.model.entity.Usuario;
 import com.escola.escola_api.repository.UsuarioRepository;
+import com.escola.escola_api.validator.UsuarioValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -14,8 +15,10 @@ public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
     private final PasswordEncoder passwordEncoder;
+    private final UsuarioValidator validator;
 
     public void salvar(Usuario usuario) {
+        validator.validar(usuario);
         usuario.setSenha(passwordEncoder.encode(usuario.getSenha()));
         usuarioRepository.save(usuario);
     }
