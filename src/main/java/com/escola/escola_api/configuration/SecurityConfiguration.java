8c64f7ea84bd2ac28026a3cc56c5c79aabbdb328
Miguel Wainstein <miguel.wainst@gmail.com>
2026-08-02 16:44:32 -0300

    fix: desabilitando a anotação @Bean no método userDetailsService em SecurityConfiguration

--- src/main/java/com/escola/escola_api/configuration/SecurityConfiguration.java
+++ src/main/java/com/escola/escola_api/configuration/SecurityConfiguration.java
@@ -38,7 +38,7 @@ public class SecurityConfiguration {
         return new BCryptPasswordEncoder(10);
     }
 
-    @Bean
+//    @Bean
     public UserDetailsService userDetailsService(UsuarioService usuarioService) {
         return new CustomUserDetailService(usuarioService);
     }
