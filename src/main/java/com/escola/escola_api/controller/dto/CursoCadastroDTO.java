f14f59933db68b150939104e2e6fdc445e903dad
Miguel Wainstein <miguel.wainst@gmail.com>
2026-08-03 13:02:54 -0300

    feat: atualizar método salvar em ProfessorController para retornar URI da matrícula

--- src/main/java/com/escola/escola_api/controller/dto/CursoCadastroDTO.java
+++ src/main/java/com/escola/escola_api/controller/dto/CursoCadastroDTO.java
@@ -1,10 +1,13 @@
 package com.escola.escola_api.controller.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record CursoCadastroDTO(
         @NotBlank(message = "O nome não pode ser nulo.")
+        @Size(max = 70, message = "O nome não pode ter mais de 70 caracteres")
         String nome,
+        @Size(min = 0, max = 1000, message = "A carga horária não pode ser negativa")
         Integer cargaHoras
 ) {
 }
