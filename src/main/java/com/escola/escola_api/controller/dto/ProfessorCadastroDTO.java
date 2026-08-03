fa0d40950923b0df5f1c117073887532201bfbb5
Miguel Wainstein <miguel.wainst@gmail.com>
2026-08-02 23:46:41 -0300

    fix: remover importação desnecessária de Max em ProfessorCadastroDTO

--- src/main/java/com/escola/escola_api/controller/dto/ProfessorCadastroDTO.java
+++ src/main/java/com/escola/escola_api/controller/dto/ProfessorCadastroDTO.java
@@ -1,6 +1,5 @@
 package com.escola.escola_api.controller.dto;
 
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
