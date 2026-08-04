5e7ee4c21af5d8356cf89e8318c599af27b4c6f0
Miguel Wainstein <miguel.wainst@gmail.com>
2026-08-03 23:20:51 -0300

    feat: atualizar validações em CursoCadastroDTO para carga horária obrigatória e limites

--- src/main/java/com/escola/escola_api/controller/dto/CursoCadastroDTO.java
+++ src/main/java/com/escola/escola_api/controller/dto/CursoCadastroDTO.java
@@ -1,13 +1,14 @@
 package com.escola.escola_api.controller.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public record CursoCadastroDTO(
         @NotBlank(message = "O nome não pode ser nulo.")
         @Size(max = 70, message = "O nome não pode ter mais de 70 caracteres")
         String nome,
-        @Size(min = 0, max = 1000, message = "A carga horária não pode ser negativa")
+        @NotNull(message = "A carga horária é obrigatória")
+        @Min(value = 1, message = "A carga horária deve ser de no mínimo 1 hora")
+        @Max(value = 1000, message = "A carga horária não pode ultrapassar 1000 horas")
         Integer cargaHoras
 ) {
 }
