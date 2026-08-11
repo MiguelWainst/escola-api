eefffbacd485d5d27344fcff0e4665ad843860a9
Miguel Wainstein <miguel.wainst@gmail.com>
2026-08-11 12:29:28 -0300

    feat: adicionando propriedades de regras de negócio para limites de alunos e cursos

---
+++ src/main/java/com/escola/escola_api/configuration/RegraNegocioProperties.java
@@ -0,0 +1,23 @@
+package com.escola.escola_api.configuration;
+
+import jakarta.validation.constraints.Min;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "regras-negocio")
+public class RegraNegocioProperties {
+
+    @Min(1)
+    private int maxAlunosPorCurso;
+
+    @Min(1)
+    private int maxCursoPorProfessor;
+
+    @Min(1)
+    private int maxProfessorPorCurso;
+}
