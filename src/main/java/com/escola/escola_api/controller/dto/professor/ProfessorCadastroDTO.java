b1c0c3b30b3506b9b7521d043bc27cbfa84e0c04
Miguel Wainstein <miguel.wainst@gmail.com>
2026-08-13 13:27:51 -0300

    refactor: update ProfessorCadastroDTO to use validation constants for nome and CPF

--- src/main/java/com/escola/escola_api/controller/dto/professor/ProfessorCadastroDTO.java
+++ src/main/java/com/escola/escola_api/controller/dto/professor/ProfessorCadastroDTO.java
@@ -1,6 +1,7 @@
 package com.escola.escola_api.controller.dto.professor;
 
 import com.escola.escola_api.validator.DominioEmailValido;
+import com.escola.escola_api.validator.ValidConst;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.*;
 import org.hibernate.validator.constraints.br.CPF;
@@ -11,17 +12,16 @@ import java.util.UUID;
 
 public record ProfessorCadastroDTO(
         @NotBlank(message = "O nome é obrigatório")
-        @Size(max = 70, message = "O nome não pode ter mais de 70 caracteres")
+        @Size(max = ValidConst.NOME_MAX, min = ValidConst.NOME_MIN, message = "O nome deve ter entre {min} e {max} caracteres")
         String nome,
         @NotBlank(message = "O email é obrigatório")
-        @Size(max = 50, message = "O email não pode ter mais de 50 caracteres")
         @Email(message = "O email deve ser válido")
         @DominioEmailValido(message = "O domínio do email é inválido")
         String email,
         @Past(message = "A data de nascimento deve ser uma data do passado") // 👈 Impede datas futuras!
         @JsonFormat(pattern = "yyyy-MM-dd")
         LocalDate dataNascimento,
-        @Size(max = 14, min = 11, message = "O CPF não pode ter mais de 14 caracteres")
+        @Size(max = ValidConst.CPF_MAX, min = ValidConst.CPF_MIN, message = "CPF deve estar entre {min} e {max} caracteres")
         @CPF(message = "CPF inválido. Certifique-se de enviar os 11 dígitos corretos")
         String cpf,
         List<UUID> idCurso
