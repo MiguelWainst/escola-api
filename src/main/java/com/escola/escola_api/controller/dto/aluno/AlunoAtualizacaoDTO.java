762fd52a1b4d39283c389a3ee7928866d9ee1e3f
Miguel Wainstein <miguel.wainst@gmail.com>
2026-08-13 13:04:34 -0300

    feat: create AlunoAtualizacaoDTO and add validation constants for name and CPF

--- src/main/java/com/escola/escola_api/controller/dto/aluno/AlunoAtualizacaoDTO.java
+++ src/main/java/com/escola/escola_api/controller/dto/aluno/AlunoAtualizacaoDTO.java
@@ -2,26 +2,23 @@ package com.escola.escola_api.controller.dto.aluno;
 
 import com.escola.escola_api.validator.DominioEmailValido;
 import com.escola.escola_api.validator.ValidacaoConstantes;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import org.hibernate.validator.constraints.br.CPF;
 
 import java.time.LocalDate;
-import java.util.UUID;
 
-public record AlunoCadastroDTO(
-        @NotBlank(message = "O nome é obrigatório")
+public record AlunoAtualizacaoDTO(
         @Size(max = ValidacaoConstantes.NOME_MAX, min = ValidacaoConstantes.NOME_MIN, message = "O nome deve ter entre {min} e {max} caracteres")
         String nome,
         @Email
         @DominioEmailValido(message = "O domínio do email é inválido")
-        @NotBlank(message = "O email é obrigatório")
         String email,
         @Past(message = "A data de nascimento deve ser uma data passada")
         LocalDate dataNascimento,
         @CPF(message = "O CPF é inválido")
-        @NotBlank(message = "O CPF é obrigatório")
         @Size(max = ValidacaoConstantes.CPF_MAX, min = ValidacaoConstantes.CPF_MIN, message = "CPF deve estar entre {min} e {max} caracteres")
-        String cpf,
-        UUID idCurso
+        String cpf
 ) {
 }
---
+++ src/main/java/com/escola/escola_api/validator/ValidacaoConstantes.java
@@ -0,0 +1,11 @@
+package com.escola.escola_api.validator;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public final class ValidacaoConstantes {
+    public static final int NOME_MIN = 2;
+    public static final int NOME_MAX = 100;
+    public static final int CPF_MIN = 11;
+    public static final int CPF_MAX = 14;
+}
