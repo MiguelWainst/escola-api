36d188136294d43dcd3492fe4b7dda26e12ee314
Miguel Wainstein <miguel.wainst@gmail.com>
2026-08-12 22:38:30 -0300

    feat: adicionar suporte a especificações e filtros na listagem de alunos

--- src/main/java/com/escola/escola_api/repository/AlunoRepository.java
+++ src/main/java/com/escola/escola_api/repository/AlunoRepository.java
@@ -2,10 +2,11 @@ package com.escola.escola_api.repository;
 
 import com.escola.escola_api.model.entity.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.Optional;
 
-public interface AlunoRepository extends JpaRepository<Aluno, Integer> {
+public interface AlunoRepository extends JpaRepository<Aluno, Integer>, JpaSpecificationExecutor<Aluno> {
 
     Optional<Aluno> findByMatricula(Integer matricula);
     Optional<Aluno> findByEmail(String email);
---
+++ src/main/java/com/escola/escola_api/repository/specification/AlunoSpec.java
@@ -0,0 +1,23 @@
+package com.escola.escola_api.repository.specification;
+
+import com.escola.escola_api.model.entity.Aluno;
+import jakarta.persistence.criteria.Predicate;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class AlunoSpec {
+
+    public static Specification<Aluno> comFiltros(Integer matricula, String nome, String cpf, UUID usuarioAtualizacao) {
+        return  (root, query, cb) -> {
+            List<Predicate> predicates = new ArrayList<>();
+            if (matricula != null) predicates.add(cb.like(root.get("matricula"), matricula + "%"));
+            if (nome != null && !nome.isBlank()) predicates.add(cb.like(cb.lower(root.get("nome")), "%" + nome.toLowerCase() + "%"));
+            if (cpf != null && !cpf.isBlank()) predicates.add(cb.equal(root.get("cpf"), cpf));
+            if (usuarioAtualizacao != null) predicates.add(cb.equal(root.get("usuarioAtualizacao"), usuarioAtualizacao));
+            return cb.and(predicates.toArray(Predicate[]::new));
+        };
+    }
+}
