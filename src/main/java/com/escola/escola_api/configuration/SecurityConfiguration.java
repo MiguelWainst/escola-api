2673b74e308c8fba0c4dbdeeaccb791218d23622
Miguel Wainstein <miguel.wainst@gmail.com>
2026-08-14 09:31:47 -0300

    fix: add AuthenticationManager bean to SecurityConfiguration for improved authentication handling

--- src/main/java/com/escola/escola_api/configuration/SecurityConfiguration.java
+++ src/main/java/com/escola/escola_api/configuration/SecurityConfiguration.java
@@ -1,9 +1,12 @@
 package com.escola.escola_api.configuration;
 
+import org.mapstruct.BeanMapping;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -34,5 +37,10 @@ public class SecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(10);
+    }
+
+    @BeanMapping
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
