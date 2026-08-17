ac2ac82fd1248add46f201ab26202c0faf81088e
Miguel Wainstein <miguel.wainst@gmail.com>
2026-08-16 21:53:30 -0300

    fix: update validation constraints in UsuarioLoginDTO to use constants from ValidConst

--- src/main/java/com/escola/escola_api/controller/dto/usuario/UsuarioLoginDTO.java
+++ src/main/java/com/escola/escola_api/controller/dto/usuario/UsuarioLoginDTO.java
@@ -1,16 +1,18 @@
 package com.escola.escola_api.controller.dto.usuario;
 
+import com.escola.escola_api.validator.ValidConst;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record UsuarioLoginDTO(
+
         @NotBlank(message = "O usuário é obrigatório")
-        @Size(min = 3, max = 30, message = "O username deve ter entre 3 e 30 caracteres")
+        @Size(min = ValidConst.USUARIO_MIN, max = ValidConst.USUARIO_MAX, message = "O username deve ter entre {min} e {max} caracteres")
         String username,
 
         @NotBlank(message = "A senha é obrigatória")
-        @Size(min = 8, max = 100, message = "A senha deve ter no mínimo 8 caracteres")
+        @Size(min = ValidConst.SENHA_MIN, max = ValidConst.SENHA_MAX, message = "A senha deve ter no mínimo {min} caracteres")
         @Pattern(regexp = ".*[0-9].*", message = "A senha deve ter pelo menos um número")
         String senha
 ) {
