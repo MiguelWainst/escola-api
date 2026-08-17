0ac93f8a1904d368e33c59cc22caab8900783e8f
Miguel Wainstein <miguel.wainst@gmail.com>
2026-08-17 01:05:46 -0300

    feat: adicionando logica para checar dono da conta

--- src/main/java/com/escola/escola_api/security/SecurityService.java
+++ src/main/java/com/escola/escola_api/security/SecurityService.java
@@ -1,6 +1,8 @@
 package com.escola.escola_api.security;
 
 import com.escola.escola_api.model.entity.Usuario;
+import com.escola.escola_api.repository.AlunoRepository;
+import com.escola.escola_api.repository.ProfessorRepository;
 import com.escola.escola_api.repository.UsuarioRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
@@ -8,11 +10,15 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
 @Component
 @RequiredArgsConstructor
 public class SecurityService {
 
     private final UsuarioRepository usuarioRepository;
+    private final AlunoRepository alunoRepository;
+    private final ProfessorRepository professorRepository;
 
     public Usuario obterUsuarioLogado() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -27,5 +33,30 @@ public class SecurityService {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null) return false;
         return authentication.getAuthorities().stream().anyMatch(auth -> auth.getAuthority().equals("ROLE_ADMIN"));
+    }
+
+    public boolean isDonoDoAluno(Integer matricula) {
+        UUID idUsuarioLogado = obterIdUsuarioLogado();
+        if (idUsuarioLogado == null) return false;
+        return alunoRepository.findByMatricula(matricula)
+                .map(aluno -> idUsuarioLogado.equals(aluno.getIdUsuario()))
+                .orElse(false);
+    }
+
+    public boolean isDonoDoProfessor(Integer matricula) {
+        UUID idUsuarioLogado = obterIdUsuarioLogado();
+        if (idUsuarioLogado == null) return false;
+        return professorRepository.findByMatricula(matricula)
+                .map(professor -> idUsuarioLogado.equals(professor.getIdUsuario()))
+                .orElse(false);
+    }
+
+    public UUID obterIdUsuarioLogado() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null) return null;
+        if (authentication.getPrincipal() instanceof CustomPrincipal principal) {
+            return principal.id();
+        }
+        return null;
     }
 }
