0ebe42c0df2ab159f1d49ae65e12fc8d9d7bb8f6
Miguel Wainstein <miguel.wainst@gmail.com>
2026-08-18 00:13:14 -0300

    fix: corrigindo comparação de matrícula e carga horária nas especificações de Aluno e Curso

--- src/main/java/com/escola/escola_api/repository/specification/AlunoSpec.java
+++ src/main/java/com/escola/escola_api/repository/specification/AlunoSpec.java
@@ -13,7 +13,7 @@ public class AlunoSpec {
     public static Specification<Aluno> comFiltros(Integer matricula, String nome, String cpf, UUID usuarioAtualizacao) {
         return  (root, query, cb) -> {
             List<Predicate> predicates = new ArrayList<>();
-            if (matricula != null) predicates.add(cb.like(root.get("matricula"), matricula + "%"));
+            if (matricula != null) predicates.add(cb.equal(root.get("matricula"), matricula));
             if (nome != null && !nome.isBlank()) predicates.add(cb.like(cb.lower(root.get("nome")), "%" + nome.toLowerCase() + "%"));
             if (cpf != null && !cpf.isBlank()) predicates.add(cb.equal(root.get("cpf"), cpf));
             if (usuarioAtualizacao != null) predicates.add(cb.equal(root.get("usuarioAtualizacao"), usuarioAtualizacao));
